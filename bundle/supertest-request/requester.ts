0431cd715c07878cce10591e0df232556f4f4951
yiy0ung <wlsdud2194@gmail.com>
2025-02-04 17:46:21 +0900

    fix: import expression

--- bundle/supertest-request/requester.ts
+++ bundle/supertest-request/requester.ts
@@ -1,4 +1,4 @@
-import * as supertest from 'supertest';
+import supertest from 'supertest';
 import { SupertestClientConnectionDefaults, RequesterConnection } from './requester-connection';
 
 export namespace Requester {
